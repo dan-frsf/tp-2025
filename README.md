2f549224dcfeb1b10e65b8a782fd1e1a0aa8e58a
mdominguez <mdominguez@santafe.gov.ar>
2025-05-01 14:29:05 -0300

    actualizo documentacion y readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # tp-2025
 Trabajo práctico DAN 2025
+
+
+# Organizacion de directorios
+
+Este proyecto es un MONOREPO (es decir tendremos todos los elementos necesarios para ejecutar la aplicacion distribuida en un único repositorio)
+
+## Infra
+Contiene los archivos docker para iniciar los servicios de infraestructura (gateway - bases de datos - etc)
+
+## Services
+Tendremos los microservicios spring boot
+
+
+# Ejecucion
+Para levantar los servicios de infra
+
+### MySQL y phpmyadmin
+```
+docker compose -f infra/docker-compose.yml up -d mysql phpmyadmin
+````
+
+```
+docker compose -f infra/docker-compose.yml down -v
+```
---
+++ infra/mysql/initdb/01_schema.sql
@@ -0,0 +1,40 @@
+-- Script para crear las tablas en el esquema `users`
+CREATE SCHEMA IF NOT EXISTS users;
+
+CREATE TABLE users.bancos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users.cuentas_bancarias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero_cuenta VARCHAR(255) NOT NULL,
+    cbu VARCHAR(255) NOT NULL,
+    alias VARCHAR(255),
+    banco_id INT,
+    FOREIGN KEY (banco_id) REFERENCES users.bancos(id)
+);
+
+CREATE TABLE users.usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefono VARCHAR(255) NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
+    fecha_nacimiento DATE,
+    cuenta_bancaria_id INT,
+    hotel_id INT,
+    FOREIGN KEY (cuenta_bancaria_id) REFERENCES users.cuentas_bancarias(id)
+);
+
+
+CREATE TABLE users.tarjetas_credito (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero_tarjeta VARCHAR(22) NOT NULL,
+    nombre_titular VARCHAR(255) NOT NULL,
+    fecha_vencimiento VARCHAR(4) NOT NULL,
+    codigo_seguridad VARCHAR(4) NOT NULL,
+    es_principal BIT(1),
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES users.usuarios(id)
+);
